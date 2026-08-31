1d7fd81f9deba34ea932af01e18d7c33d70705c0
taechatno <taechatno@gmail.com>
2026-08-31 20:44:55 +0700

    Update nubnub.lua

--- nubnub.lua
+++ nubnub.lua
@@ -1,1 +1,26 @@
-# nubnub
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+
+local blacklist = {
+    ["im_mythaw"] = true,
+    ["PlayerName2"] = true,
+    ["PlayerName3"] = true
+}
+
+local player = Players.LocalPlayer
+
+local function checkServer()
+    for _, otherPlayer in ipairs(Players:GetPlayers()) do
+        if otherPlayer ~= player and blacklist[otherPlayer.Name] then
+            TeleportService:Teleport(game.PlaceId, player)
+            return
+        end
+    end
+end
+
+checkServer()
+
+Players.PlayerAdded:Connect(function()
+    task.wait(1)
+    checkServer()
+end)
